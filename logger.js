a39d66a4eb3fd0e3794d225f0220c54e4b7670fd
Guc0d3 <guc0d3@gmail.com>
2019-01-08 18:22:31 +0700

    set logger

--- logger.js
+++ logger.js
@@ -21,7 +21,28 @@
 
 // module.exports = logger
 
-const winston = require('winston')
+// const winston = require('winston')
+
+// const transports = {
+//   console: new winston.transports.Console({ level: process.env.LOG_LEVEL }),
+//   file: new winston.transports.File({
+//     filename: 'combined.log',
+//     level: 'error'
+//   })
+// }
+
+// const logger = winston.createLogger({
+//   transports: [transports.console, transports.file]
+// })
+
+// module.exports = logger
+
+const { createLogger, format, transports } = require('winston')
+const { combine, timestamp, printf, colorize } = format
+
+const myFormat = printf(info => {
+  return `${info.timestamp} ${info.level}: ${info.message}`
+})
 
 const transports = {
   console: new winston.transports.Console({ level: process.env.LOG_LEVEL }),
@@ -31,8 +52,15 @@ const transports = {
   })
 }
 
-const logger = winston.createLogger({
-  transports: [transports.console, transports.file]
+const logger = createLogger({
+  format: combine(colorize(), timestamp(), myFormat),
+  transports: [transports.console, transports.file],
+  exitOnError: false
 })
 
 module.exports = logger
+module.exports.stream = {
+  write: function(message, encoding) {
+    logger.info(message)
+  }
+}
