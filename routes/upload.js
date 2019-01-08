118e11d7af0205dc6233022a46a5495c9fbc1e78
Guc0d3 <guc0d3@gmail.com>
2019-01-09 00:59:50 +0700

    split upload aws file

--- routes/upload.js
+++ routes/upload.js
@@ -1,36 +1,11 @@
-const AWS = require('aws-sdk')
 const express = require('express')
-const Multer = require('multer')
-const MulterS3 = require('multer-s3')
 const logger = require('../logger')
-
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
-})
-
-const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
-
-const upload = Multer({
-  storage: MulterS3({
-    // acl: 'public-read',
-    bucket: process.env.AWS_S3_BUCKET_NAME,
-    // contentType: MulterS3.AUTO_CONTENT_TYPE,
-    s3,
-    key: (req, file, cb) => {
-      // cb(null, 'files/' + Date.now().toString() + '-' + file.originalname)
-      logger.debug('test upload')
-      cb(null, Date.now().toString() + '-' + file.originalname)
-    }
-  })
-})
+const upload = require('../upload')
 
 const router = express.Router()
 router.post('/', upload.single('file'), function(req, res) {
   logger.debug('POST /upload')
-  logger.debug('req.file:')
-  logger.debug(req.file)
+  logger.debug('req.file: ' + JSON.stringify(req.file, null, 2))
   try {
     if (!req.file) {
       throw new Error('No file received')
---
+++ upload.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk')
+const Multer = require('multer')
+const MulterS3 = require('multer-s3')
+const logger = require('../logger')
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+})
+
+const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
+
+const upload = Multer({
+  storage: MulterS3({
+    // acl: 'public-read',
+    bucket: process.env.AWS_S3_BUCKET_NAME,
+    // contentType: MulterS3.AUTO_CONTENT_TYPE,
+    s3,
+    key: (req, file, cb) => {
+      logger.debug(`file: ${JSON.stringify(file, null, 2)}`)
+      cb(null, 'files/' + Date.now().toString() + '-' + file.originalname)
+    }
+  })
+})
+
+module.exports = upload
