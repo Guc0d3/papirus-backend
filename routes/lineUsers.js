5f0a3d374463e60f9dc072cb02e70e162ca60547
Guc0d3 <guc0d3@gmail.com>
2019-01-13 08:14:19 +0700

    fix snakeCase to camelCase

--- routes/lineUsers.js
+++ routes/lineUsers.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const moment = require('moment')
 const _ = require('lodash')
 const db = require('../db')
+const logger = require('../logger')
 
 const router = express.Router()
 router
@@ -19,10 +21,9 @@ router
           req.query.search
         )
         .selectPagination(req.query)
-      const data = rows.map(row =>
-        _.mapKeys(row, (value, key) => _.camelCase(key))
-      )
-      res.status(200).json(data)
+      res
+        .status(200)
+        .json(rows.map(row => _.mapKeys(row, (value, key) => _.camelCase(key))))
     } catch (err) {
       console.dir(err)
       res.sendStatus(500)
@@ -31,20 +32,20 @@ router
   .get('/:id', async (req, res) => {
     try {
       const rows = await db('line_users').where('id', parseInt(req.params.id))
-      const data = _.mapKeys(rows[0], (value, key) => _.camelCase(key))
-      res.status(200).json(data)
+      res.status(200).json(_.mapKeys(rows[0], (value, key) => _.camelCase(key)))
     } catch (err) {
       console.dir(err)
       res.sendStatus(500)
     }
   })
   .patch('/:id', (req, res) => {
+    logger.debug('req.body: ' + JSON.stringify(req.body, null, 2))
     db.transaction(async trx => {
       const rows = await trx('line_users')
         .update({
           name: req.body.name,
-          group_code: req.body['group_code'],
-          expired_at: req.body['expired_at'],
+          group_code: req.body.groupCode,
+          expired_at: req.body.expiredAt,
           updated_at: db.fn.now()
         })
         .where('id', parseInt(req.params.id))
